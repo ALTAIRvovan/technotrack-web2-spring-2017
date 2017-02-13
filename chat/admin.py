2b779b08077c5e480efd6c5a2bb8586399f35696
Vladimir Panov <vladimir.panov@phystech.edu>
2017-02-13 23:18:45 +0300

    добавил простую админку. Без особых настроек

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat.models import Message, Chat, ChatMembership
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('author', 'chat', 'subject')
+    list_display_links = ('chat',)
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author')
+
+
+@admin.register(ChatMembership)
+class ChatMembershipAdmin(admin.ModelAdmin):
+    pass
--- friendship/admin.py
+++ friendship/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from friendship.models import *
+
+
+@admin.register(FriendShip)
+class FriendShipAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Friends)
+class FriendsAdmin(admin.ModelAdmin):
+    pass
