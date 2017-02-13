d3ea7970beace7b04b5769b9ef2854e5ace7c55f
Vladimir Panov <vladimir.panov@phystech.edu>
2017-02-13 20:19:53 +0300

    Добавил модели Дружбы

--- friendship/models.py
+++ friendship/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from core.models import User
+from ugc.models import Authored, Dated
+
+
+class FriendShip(models.Model, Authored, Dated):
+    recipient = models.ForeignKey(User)
+    approved = models.BooleanField()
+
+
+class Friends(models.Model, Authored):
+    friendShip = models.ForeignKey(FriendShip)
+    friend = models.ForeignKey(User)
