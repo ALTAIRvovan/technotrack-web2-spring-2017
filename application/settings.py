5e2e089ac2b7533bd87e0d892ff38abc9e559ab1
Vladimir Panov <vladimir.panov@phystech.edu>
2017-02-13 19:33:13 +0300

    добавил разрешенный host для работы через nginx

--- application/settings.py
+++ application/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config.get('main', 'SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['track']
 
 
 # Application definition
