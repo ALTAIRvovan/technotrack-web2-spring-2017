beb7a2847655ae5c1916baa9e1ced08fb7ff09b6
Vladimir Panov <vladimir.panov@phystech.edu>
2017-03-14 18:17:02 +0300

    filter feed api

--- feed/api.py
+++ feed/api.py
@@ -6,8 +6,17 @@ from .models import Feed
 
 class FeedViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = FeedSerializer
+    lookup_field = 'author'
 
     def get_queryset(self):
-        return Feed.objects.filter(author__friends__friend=self.request.user)
+        q = Feed.objects.filter(author__friends__friend=self.request.user)
+        friend_id = self.kwargs.get(self.lookup_field, None)
+        if friend_id is not None:
+            q = q.filter(author_id=int(friend_id))
+        return q
+
+    def retrieve(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
 
 router.register("feed", FeedViewSet, "feed")
