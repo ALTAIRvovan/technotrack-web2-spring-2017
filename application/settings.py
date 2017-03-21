627afbed683a79ca22de85355b7c1eb1e2b4b475
Vladimir Panov <vladimir.panov@phystech.edu>
2017-03-21 12:38:06 +0300

    Сменил базу на Postgres

--- application/settings.py
+++ application/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config.get('db', 'NAME'),
         'USER': config.get('db', 'USER'),
         'PASSWORD': config.get('db', 'PASSWORD'),
