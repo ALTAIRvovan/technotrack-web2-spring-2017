22071c7691c21df386f00c88c9ce8f911b55dd7f
Vladimir Panov <vladimir.panov@phystech.edu>
2017-03-23 09:58:28 +0300

    первая настройка webpack

---
+++ frontend/assets/js/Friends.jsx
@@ -0,0 +1,11 @@
+/**
+ * Created by altair on 23.03.17.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'containers/App';
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
